a043401aa4cdc97b6e70891b924793275f3f8319
maryamzain123 <smaryam.zain@gmail.com>
2021-04-18 07:17:43 -0500

    updated

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -18,18 +18,84 @@ class EntriesController < ApplicationController
         erb :"entries/monthlist" 
     end
 
+    get "/entries/january" do
+        @entries = Entry.all
+        @january_entries = @entries.filter {|ent| ent.month=="january"}
+        @january_total = @january_entries.map {|ent| ent.amount}.sum
+        erb :"entries/january"
+    end
+    get "/entries/february" do
+        @entries = Entry.all
+        @february_entries = @entries.filter {|ent| ent.month=="february"}
+        @february_total = @february_entries.map {|ent| ent.amount}.sum
+        erb :"entries/february"
+    end
+    get "/entries/march" do
+        @entries = Entry.all
+        @march_entries = @entries.filter {|ent| ent.month=="march"}
+        @march_total = @march_entries.map {|ent| ent.amount}.sum
+        erb :"entries/march"
+    end
+    get "/entries/april" do
+        @entries = Entry.all
+        @april_entries = @entries.filter {|ent| ent.month=="april"}
+        @april_total = @april_entries.map {|ent| ent.amount}.sum
+        erb :"entries/april"
+    end
+    get "/entries/may" do
+        @entries = Entry.all
+        @may_entries = @entries.filter {|ent| ent.month=="may"}
+        @may_total = @may_entries.map {|ent| ent.amount}.sum
+        erb :"entries/may"
+    end
+    get "/entries/june" do
+        @entries = Entry.all
+        @june_entries = @entries.filter {|ent| ent.month=="june"}
+        @june_total = @june_entries.map {|ent| ent.amount}.sum
+        erb :"entries/june"
+    end
+
+    get "/entries/july" do
+        @entries = Entry.all
+        @july_entries = @entries.filter {|ent| ent.month=="july"}
+        @july_total = @july_entries.map {|ent| ent.amount}.sum
+        erb :"entries/july"
+    end
+
+    get "/entries/august" do
+        @entries = Entry.all
+        @august_entries = @entries.filter {|ent| ent.month=="august"}
+        @august_total = @august_entries.map {|ent| ent.amount}.sum
+        erb :"entries/august"
+    end
+    
     get "/entries/september" do
         @entries = Entry.all
         @september_entries = @entries.filter {|ent| ent.month=="september"}
         @september_total = @september_entries.map {|ent| ent.amount}.sum
         erb :"entries/september"
     end
-    get "/entries/january" do
+
+    get "/entries/october" do
         @entries = Entry.all
-        @january_entries = @entries.filter {|ent| ent.month=="january"}
-        @january_total = @january_entries.map {|ent| ent.amount}.sum
-        erb :"/sentries/january"
+        @october_entries = @entries.filter {|ent| ent.month=="october"}
+        @october_total = @october_entries.map {|ent| ent.amount}.sum
+        erb :"entries/october"
     end
+    get "/entries/november" do
+        @entries = Entry.all
+        @november_entries = @entries.filter {|ent| ent.month=="november"}
+        @november_total = @november_entries.map {|ent| ent.amount}.sum
+        erb :"entries/november"
+    end
+    get "/entries/december" do
+        @entries = Entry.all
+        @december_entries = @entries.filter {|ent| ent.month=="december"}
+        @december_total = @december_entries.map {|ent| ent.amount}.sum
+        erb :"entries/december"
+    end
+
+
     
 
     get "/entries/:id" do
